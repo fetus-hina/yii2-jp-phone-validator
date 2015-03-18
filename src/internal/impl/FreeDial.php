7630616f62eb355e3d09f20c69a20ec4281abae0
AIZAWA, Hina <hina@bouhime.com>
2015-03-19 02:44:58 +0900

    update doc comment

--- src/internal/impl/FreeDial.php
+++ src/internal/impl/FreeDial.php
@@ -2,7 +2,7 @@
 /**
  * @author AIZAWA Hina <hina@bouhime.com>
  * @copyright 2015 by AIZAWA Hina <hina@bouhime.com>
- * @license https://github.com/fetus-hina/yii2-extra-validator/blob/master/LICENSE MIT
+ * @license https://github.com/fetus-hina/yii2-jp-phone-validator/blob/master/LICENSE MIT
  * @since 1.2.0
  */
 
