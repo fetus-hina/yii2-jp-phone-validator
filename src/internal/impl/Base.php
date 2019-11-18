12f1206d406c9edc907f76bb1d9d1c3154bfc53f
AIZAWA Hina <hina@fetus.jp>
2019-11-19 04:53:21 +0900

    Update dependencies, require PHP 7.1

--- src/internal/impl/Base.php
+++ src/internal/impl/Base.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @author AIZAWA Hina <hina@bouhime.com>
- * @copyright 2015 by AIZAWA Hina <hina@bouhime.com>
+ * @author AIZAWA Hina <hina@fetus.jp>
+ * @copyright 2015 by AIZAWA Hina <hina@fetus.jp>
  * @license https://github.com/fetus-hina/yii2-jp-phone-validator/blob/master/LICENSE MIT
  * @since 1.0.0
  */
 
 namespace jp3cki\yii2\jpphone\internal\impl;
 
-use yii\base\Object;
+use yii\base\BaseObject;
 
-abstract class Base extends Object
+abstract class Base extends BaseObject
 {
     /**
      * Hyphen accept/require mode
